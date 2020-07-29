53478d83d22c995c2a978f6333ebc9dc460c2cfb
langpu <15937237056@163.com>
2020-07-29 15:25:01 +0800

    commit

---
+++ TextViewDemo/app/src/main/java/io/yovelas/MainActivity.java
@@ -0,0 +1,22 @@
+package io.yovelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        TextView textView = new TextView(this);
+        textView.setText("This is TextView write by programming");
+        linearLayout.addView(textView);
+    }
+}
---
+++ WebViewDemo/app/src/main/java/io/yovelas/MainActivity.java
@@ -0,0 +1,24 @@
+package io.yovelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+
+        // inner Content
+//        webView.loadData("<h1>Welcome to the web page!</h1>","text/html","UTF-8");
+
+        // external Content
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.baidu.com/");
+    }
+}
